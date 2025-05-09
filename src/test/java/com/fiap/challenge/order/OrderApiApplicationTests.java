836222d5e4483ce7d71917470ee2bfb75d4b1c23
Wiliam Hatimine <wiliamhatimine@gmail.com>
2025-05-09 17:18:58 -0300

    Fixing unit tests for SqsTemplate

--- src/test/java/com/fiap/challenge/order/OrderApiApplicationTests.java
+++ src/test/java/com/fiap/challenge/order/OrderApiApplicationTests.java
@@ -2,10 +2,16 @@ package com.fiap.challenge.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
 class OrderApiApplicationTests {
 
+    @MockBean
+    private SqsTemplate sqsTemplate;
+	
 	@Test
 	void contextLoads() {
 	}
